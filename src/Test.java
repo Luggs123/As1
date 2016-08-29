5cf352c21e12018584866077788097af6311461f
CommanderMark <markjarjour@gmail.com>
2016-08-29 19:30:05 -0400

    Testing comitting.

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,7 @@ public class Test {
 	//The purpose of this class is to add the bin and src folders to the repo
 	public static void main(String[] args){
 		int x = 1;
-		System.out.print(x);
+		System.out.println(x);
+		System.out.println("Alright listen dudes. I don't know how to explain this, but I need you to shove this repo waaaaaaaay up your buttholes.");
 	}
 }
