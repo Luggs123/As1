e8ed36a6b4cf7847ac294cf1b68b004e760199fd
Luggs123 <sunsplashin@hotmail.com>
2016-08-29 19:25:41 -0400

    Sending test code with class

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+	//The purpose of this class is to add the bin and src folders to the repo
+	public static void main(String[] args){
+		int x = 1;
+		System.out.print(x);
+	}
+}
