5d93b2e3fdef015e81db66556e06599f84b3135d
shiftgenesis <pgaudnik@gmail.com>
2016-09-15 22:25:44 -0400

    Employee methods WIP#1
    minor changes, I ran into some issues decided to leave it for tomorrow and discuss

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-
-public class Test {
-	//The purpose of this class is to add the bin and src folders to the repo
-	public static void main(String[] args){
-		int x = 1;
-		System.out.println(x);
-		System.out.println("Alright listen dudes memes. I don't know how to explain this, but I need you to shove this repo waaaaaaaay up your buttholes.");
-	}
-}
