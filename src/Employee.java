8aaef08e5b3392a272ff5edd2090f9330b696f8e
shiftgenesis <pgaudnik@gmail.com>
2016-09-15 22:31:59 -0400

    Employee Methods WIP
    Minor addition, ran into issues

--- src/Employee.java
+++ src/Employee.java
@@ -20,12 +20,33 @@ public class Employee extends AbstractEmployee {
 
     @Override
     public double computeCommission(double sales) {
-	
+        
+//        double bonusSalary;
+//	if(300 >= sales && sales <= 1000)
+//        {
+//            bonusSalary = (0.10 * sales); 
+//        }
+//        else if (sales > 1000 && sales <= 5000)   
+//        {
+//            bonusSalary = (0.05 * sales);
+//        }
+//        else if (sales > 5000 && sales <= 10000)
+//        {
+//            bonusSalary = (0.01 * sales);
     }
 
     @Override
     public boolean determineValidity(int hrs, double sales) {
-	
+        
+	boolean validity;
+        
+        if (sales < 300 && sales > 10000 )
+        {
+            validity = false;
+        }
+        else validity = true;
+        
+        return validity;
     }
     
 }
