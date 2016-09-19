a4b757813ecbe4301394cc71fd8f40cec6414cc5
CommanderMark <markjarjour@gmail.com>
2016-09-19 12:32:45 -0400

    Added try catcher to avoid implicit string-to-int casting.

--- src/UIEmployee.java
+++ src/UIEmployee.java
@@ -1,4 +1,5 @@
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -16,40 +17,6 @@ public class UIEmployee extends JFrame {
 	private static final long serialVersionUID = 1371750691536720618L;
 	public static JPanel employeePanel = new JPanel();
 
-	public UIEmployee() {
-		//		// Setting up layout.
-		//		employeePanel.setLayout(new FlowLayout() );
-		//		
-		//		// Setting up row of labels.
-		//		JLabel frankensteinsLabel = new JLabel("  Employee Name:       Hours:       Sales:       ");
-		//		
-		//		employeePanel.add(frankensteinsLabel);
-		//
-		//		// Input fields.
-		//		JTextField NameTextField = new JTextField(10);
-		//		NameTextField.setEditable(true);
-		//		employeePanel.add(NameTextField);
-		//		
-		//		JTextField HrsTextField = new JTextField(4);
-		//		HrsTextField.setEditable(true);
-		//		employeePanel.add(HrsTextField);
-		//
-		//		JTextField SalesTextField = new JTextField(6);
-		//		SalesTextField.setEditable(true);
-		//		employeePanel.add(SalesTextField);
-		//		
-		//
-		//		// Buttons
-		//		JButton addButton = new JButton("Add");
-		//		addButton.setToolTipText("Add the information provided into the database.");
-		//		employeePanel.add(addButton);
-		//		// TODO: Add action handler(s).
-		//
-		//		JButton submitButton = new JButton("Submit");
-		//		submitButton.setDefaultCapable(true);
-		//		employeePanel.add(submitButton);
-	}
-
 	public static void main (String[] args) {
 		UIEmployee OneFrame = new UIEmployee();
 		List<Employee> validEmployees = new ArrayList<Employee>();
@@ -61,7 +28,6 @@ public class UIEmployee extends JFrame {
 
 		// Setting up row of labels.
 		JLabel frankensteinsLabel = new JLabel("  Employee Name:       Hours:       Sales:       ");
-
 		employeePanel.add(frankensteinsLabel);
 
 		// Input fields.
@@ -79,10 +45,23 @@ public class UIEmployee extends JFrame {
 
 		class buttonListenerADD implements ActionListener {
 			public void actionPerformed(ActionEvent e) {
+				Boolean validInt;
+				Boolean validDouble;
+
 				String name = NameTextField.getText();
-				int hrs = Integer.parseInt(HrsTextField.getText());
-				double sales = Double.parseDouble(SalesTextField.getText());
-				employeeBuffer.add(new Employee(name, hrs, sales));
+
+				try {  
+					int hrs = Integer.parseInt(HrsTextField.getText());
+					double sales = Double.parseDouble(SalesTextField.getText());
+					employeeBuffer.add(new Employee(name, hrs, sales));
+
+					// Clear text fields.
+					NameTextField.setText("");
+					HrsTextField.setText("");
+					SalesTextField.setText("");
+				} catch (NumberFormatException nfe) {  
+					JOptionPane.showMessageDialog(null, "You implicitly casted bastard." , "Output", JOptionPane.ERROR_MESSAGE);
+				}
 			}
 		}
 
